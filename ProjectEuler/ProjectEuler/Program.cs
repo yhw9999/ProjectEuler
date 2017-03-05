8d4e283ed973131ab1cbfd803a6c6accfcb76497
Heewoong Yeom <nhyhw999@naver.com>
2017-03-06 04:04:56 +0900

    Problem 11

---
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,14 @@
+﻿using ProjectEuler.Problems;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solve solve = new Problem11();
+
+            solve.Operate();
+        }
+    }
+}
